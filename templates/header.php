49ff021cdf4bb1b1f30ed52691716b48034d6208
Gabriel Macario <moreirag1212@gmail.com>
2025-02-20 16:37:13 -0300

    cd

--- templates/header.php
+++ templates/header.php
@@ -8,6 +8,7 @@ require_once('globals.php');
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Portfólio - Gabriel Macario</title>
+    <link rel="icon" type="img/favicon.ico" href="img/favicon.ico">
     <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
