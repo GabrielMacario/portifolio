d3751ed2f64ecf99da28dadd2f4d5e0d9f8e4851
Gabriel Macario <moreirag1212@gmail.com>
2025-02-23 21:24:18 -0300

    feat: add linkedin and css

--- templates/footer.php
+++ templates/footer.php
@@ -1,4 +1,5 @@
 <footer id="footer">
     <script src="scripts/script.js"></script>
+    <script src="https://platform.linkedin.com/badges/js/profile.js" async defer type="text/javascript"></script>                      
     <p>&copy; 2025 Gabriel Macario. Todos os direitos reservados. v2</p>
 </footer>
--- templates/header.php
+++ templates/header.php
@@ -2,6 +2,12 @@
 
 require_once('globals.php');
 
+
+$screenWidth = 768;
+
+$dataSize = ($screenWidth <= 768) ? 'medium' : 'large';
+
+$dataType = ($screenWidth <= 768) ? 'VERTICAL' : 'HORIZONTAL';
 ?>
 
 <head>
@@ -13,12 +19,14 @@ require_once('globals.php');
 </head>
 <body>
 <header>
-    <h1>Gabriel Macario</h1>
-    <nav>
-        <ul>
-            <li><a href="<?= $BASE_URL ?>index.php">Home</a></li>
-            <li><a href="<?= $BASE_URL ?>#projects">Projetos</a></li>
-            <li><a href="<?= $BASE_URL ?>contacts.php">Contatos</a></li>
-        </ul>
-    </nav>
+<div id='linkedin-ico' class="badge-base LI-profile-badge" data-locale="pt_BR" data-size="<?= $dataSize ?>" data-theme="dark" data-type="<?= $dataType ?>" data-vanity="gabriel-macario-dev" data-version="v1"><a class="badge-base__link LI-simple-link" href="https://br.linkedin.com/in/gabriel-macario-dev?trk=profile-badge"></a></div>
+   <div class="name-add">
+        <nav>
+            <ul>
+                <li><a href="<?= $BASE_URL ?>index.php">Home</a></li>
+                <li><a href="<?= $BASE_URL ?>#projects">Projetos</a></li>
+                <li><a href="<?= $BASE_URL ?>/contacts.php">Contatos</a></li>
+            </ul>
+        </nav>
+    </div>
 </header>
