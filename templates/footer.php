bbb96fdf8c8e15420d7c06acf7591b6783bbaaaf
Gabriel Macario <moreirag1212@gmail.com>
2025-02-24 07:27:46 -0300

    chore: adjusting code

--- templates/footer.php
+++ templates/footer.php
@@ -1,5 +1,4 @@
 <footer id="footer">
     <script src="scripts/script.js"></script>
-    <script src="https://platform.linkedin.com/badges/js/profile.js" async defer type="text/javascript"></script>                      
     <p>&copy; 2025 Gabriel Macario. Todos os direitos reservados. v2</p>
 </footer>
